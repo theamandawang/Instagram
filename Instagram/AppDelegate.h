1ab8265c87c304786a7cc604d4ef9ecc2d4a73ac
Amanda Wang <amandawang@fb.com>
2022-06-27 12:06:56 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Amanda Wang on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
